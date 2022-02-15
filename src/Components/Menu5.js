6aac33b027cecbd93d5196e06199c09d188d7bab
RashVV <vadim.ratushnyak@gmail.com>
2022-02-15 19:47:37 +0200

    clean code frome garbige add title

--- src/Components/Menu5.js
+++ src/Components/Menu5.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+
 
 export default function Menu5() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -9,9 +8,7 @@ export default function Menu5() {
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  
 
   return (
     <div>
@@ -28,3 +25,4 @@ export default function Menu5() {
        </div>
        );
      }
+     

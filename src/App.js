d1b698c7655500baa3b51facc1fba983089342b0
RashVV <vadim.ratushnyak@gmail.com>
2022-02-15 15:52:05 +0200

    header in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Menu0 from './Components/Menu0';
+import Menu1 from './Components/Menu1';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <p>MovieRoom App</p>
+      <NavBar />
+      <Menu0/>
+      <Menu1 />
     </div>
   );
 }

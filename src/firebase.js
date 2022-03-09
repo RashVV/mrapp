ba8c94b269423835c551b47d53206bd22b787ea9
RashVV <vadim.ratushnyak@gmail.com>
2022-03-09 16:44:34 +0200

    take out firebaceconfig to ext file

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, getAuth, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, signOut, } from '@firebase/auth';
 import { getFirestore, query, getDocs, collection, where, addDoc, } from '@firebase/firestore';
-var firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    databaseURL: process.env.REACT_APP_BASEURL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
-  };
-  const app = initializeApp(firebaseConfig);
+import { firebaseConfig } from './firebaseConfig';
+
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();

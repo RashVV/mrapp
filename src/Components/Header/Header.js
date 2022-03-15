062949a28792e1cbd770bee729030199b336dee7
RashVV <vadim.ratushnyak@gmail.com>
2022-03-15 22:23:51 +0200

    resp start

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
+import { ReactComponent as CloseMenu } from './../../image/x.svg';
+import { ReactComponent as MenuIcon } from './../../image/menu.svg';
 import LoginBtn from './LoginBtn';
 import { SearchBox } from './SearchBox';
 import logo from '../../image/logo.png';
 import MenuPopups from './MenuPopups';
 
 function Header( ) {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
    <div className='container'>
-      <div className='Logo'>
-        <p>
-          <a href='/' >
-            <img src={logo} alt='MovieRoom App' />
-          </a>
-        </p>
-      </div> 
-      <div className='nav__menu'>
-        <MenuPopups /> 
-      </div>
-      <div>
-        <SearchBox />
-      </div>
-      <div className='button'>
-        <LoginBtn />
+     <div className='logo-nav'>
+        <div className='Logo'>
+          <p>
+            <a href='/' >
+              <img src={logo} alt='MovieRoom App' />
+            </a>
+          </p>
+        </div> 
+        <ul className={click ? "nav-options active" : "nav-options"}>
+          <li className="option" onClick={closeMobileMenu}> 
+              <div className='nav__menu'>
+                 <MenuPopups /> 
+              </div>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <div>
+              <SearchBox />
+            </div>
+          </li>
+        </ul>  
+        <ul className="signin-up">
+          <li className="sign-in" onClick={closeMobileMenu}>
+            <div className='button'>
+              <LoginBtn />
+            </div>
+          </li>
+        </ul>
+        <div className="mobile-menu" onClick={handleClick}>
+          {click ? (
+            <CloseMenu className="menu-icon" />
+              ) : (
+            <MenuIcon className="menu-icon" />
+          )}
+        </div>
       </div>
     </div>
   )

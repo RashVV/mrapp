7f30c90fed83c1a294ec276f29fcbeb95699bd58
AnnaZaburanna <annzaburanna@gmail.com>
2022-03-24 20:45:58 +0200

    fixed margin in mobile mode

--- src/Pages/popularMovies.js
+++ src/Pages/popularMovies.js
@@ -4,29 +4,22 @@ import {Sort} from "../Components/Sort";
 import {PopularMovieProvider} from "../context/PopularMovieProvider";
 import {Paper} from "@mui/material";
 import Grid from "@mui/material/Grid";
-import {PaginationList} from "../Components/PaginationList";
 import '../App.css';
 
 export function PopularMovies () {
   return (
     <PopularMovieProvider>
       <Paper >
-        <Grid container sx={{margin: "0 20px"}}>
+        <Grid container >
           <Grid xs={12} item>
             <p className="text">Popular movies</p>
           </Grid>
           <Grid container item xs={12} >
-            <Grid item xs={12} md={3}>
+            <Grid item xs={12} md={3} sx={{margin: "0 0 20px 0"}}>
               <Sort/>
             </Grid>
             <Grid container item xs={12} md={9}>
               <PopularMovieList/>
-            </Grid>
-            <Grid container item sx={{display: "flex",
-              justifyContent: 'center',
-              margin: "0 0 20px 20px"
-            }}>
-              <PaginationList />
             </Grid>
           </Grid>
         </Grid>

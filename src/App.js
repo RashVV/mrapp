fae1adebc1ef053c9fdeb2342f8f87d58d6abdb7
Bohdan Krymchuk <bkrymchuk@gmail.com>
2022-04-06 08:55:16 +0300

    remove broken imports

--- src/App.js
+++ src/App.js
@@ -4,29 +4,29 @@ import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
 import Reset from './Components/Reset/Reset';
 import Dashboard from './Components/Dashboard/Dashboard';
-import Favorites from './pages/Favorites';
-import Friends from './pages/Friends';
-import Allmovies from './pages/Allmovies';
-import Collections from './pages/Collections';
-import Inthemovies from './pages/Inthemovies';
-import Mycalendars from './pages/Mycalendars';
-import Myseries from './pages/Myseries';
-import News from './pages/News';
-import Online from './pages/Online';
-import Popular from './pages/Popular';
-import Premieres from './pages/Premieres';
-import Recommendations from './pages/Recommendations';
-import Reviews from './pages/Reviews';
-import Searchbyname from './pages/Searchbyname';
-import Soon from './pages/Soon';
-import Tape from './pages/Tape';
-import Trailers from './pages/Trailers';
-import TVseries from './pages/TVseries';
-import Videos from './pages/Videos';
 import Header from './Components/Header/Header';
 import Footer from "./layout/footer";
-import {MoviesPage} from "./pages/moviesPage";
-import {PopularMovies} from "./pages/popularMovies";
+// import Favorites from './pages/Favorites';
+// import Friends from './pages/Friends';
+// import Allmovies from './pages/Allmovies';
+// import Collections from './pages/Collections';
+// import Inthemovies from './pages/Inthemovies';
+// import Mycalendars from './pages/Mycalendars';
+// import Myseries from './pages/Myseries';
+// import News from './pages/News';
+// import Online from './pages/Online';
+// import Popular from './pages/Popular';
+// import Premieres from './pages/Premieres';
+// import Recommendations from './pages/Recommendations';
+// import Reviews from './pages/Reviews';
+// import Searchbyname from './pages/Searchbyname';
+// import Soon from './pages/Soon';
+// import Tape from './pages/Tape';
+// import Trailers from './pages/Trailers';
+// import TVseries from './pages/TVseries';
+// import Videos from './pages/Videos';
+// import {MoviesPage} from "./pages/moviesPage";
+// import {PopularMovies} from "./pages/popularMovies";
 
 function App() {
   return (
@@ -35,7 +35,7 @@ function App() {
       <Routes>
         <Route path='/' element={<div>Home</div>} />
         <Route exact path='/login' element={<Login />} />
-        <Route exact path="/popularMovies" element={<PopularMovies/>} />
+        {/* <Route exact path="/popularMovies" element={<PopularMovies/>} />
         <Route exact path="/movies" element={<MoviesPage/>}/>
         <Route exact path='/feed/friends' element={<Friends />} />
         <Route exact path='/R2D2' element={<Allmovies />} />
@@ -55,7 +55,7 @@ function App() {
         <Route exact path='/videos/trailers' element={<Trailers />} />
         <Route exact path='/shows' element={<TVseries />} />
         <Route exact path='/videos' element={<Videos />} />
-        <Route exact path='/feed/favorites' element={<Favorites />} />
+        <Route exact path='/feed/favorites' element={<Favorites />} /> */}
         <Route exact path='/register' element={<Register />} />
         <Route exact path='/reset' element={<Reset />} />
         <Route exact path='/dashboard' element={<Dashboard />} />

8d1fd56329615c6f34c895f337a003de0e3fad72
RashVV <vadim.ratushnyak@gmail.com>
2022-04-12 22:56:54 +0300

    start

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import './Favorites.css';
 
+const MovieCounter = 4;
+const TVCounter = 0;
 function Favorites() {
   return (
-    <div>Favorites</div>
+    <div className='page__container'>
+      <div className='page__header'>
+        <div className='page__Name'> My Favorites </div>
+        <div className='Fav__Movies'> Movies {MovieCounter} </div>
+        <div className='Fav__TV'> TV {TVCounter} </div>
+
+      
+      </div>
+    </div>
   );
 }
 

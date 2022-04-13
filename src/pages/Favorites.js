a88d2d9e610f90eb9943ed4b9c439046d6878d7d
RashVV <vadim.ratushnyak@gmail.com>
2022-04-13 14:47:55 +0300

    add right side of nav

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -7,10 +7,17 @@ function Favorites() {
   return (
     <div className='page__container'>
       <div className='page__header'>
-        <div className='page__Name'> My Favorites </div>
-        <div className='Fav__Movies'> Movies {MovieCounter} </div>
-        <div className='Fav__TV'> TV {TVCounter} </div>
-
+        <div className='left_page__header'>
+          <div className='page__Name'> My Favorites </div>
+          <div className='Fav__Movies'> Movies {MovieCounter} </div>
+          <div className='Fav__TV'> TV {TVCounter} </div>
+        </div>
+        <div className='Right_page__header'>
+          <div className='Fav__filter'> Filter by: </div>
+          <div className='Fav__filter__sort'> Date Added </div>
+          <div className='Fav__filter__order'> Order: </div>
+          <button>A</button>
+        </div>
       
       </div>
     </div>
